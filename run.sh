b3b694586e874a5433261311266fb409cdbde5cd
yunerliu <liuyunchenyuner@gmail.com>
2022-11-13 16:00:27 -0600

    inventory basic crud

--- run.sh
+++ run.sh
@@ -2,10 +2,11 @@ pwd
 
 docker stop InventoryDB || true
 docker rm InventoryDB || true
+docker rmi randomteam:inventory || true
 docker run -d --hostname inventorydb --name InventoryDB mongo
 
 docker image build -t randomteam:inventory ./inventory
 docker stop Inventory || true
 docker rm Inventory || true
-docker run -p 5000:5000 -d --hostname inventory --name Inventory --link InventoryDB:inventorydb randomteam:inventory
+docker run -p 5001:5000 -d --hostname inventory --name Inventory --link InventoryDB:inventorydb randomteam:inventory
 
