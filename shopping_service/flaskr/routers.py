bcf0e639c3eb3b13247a4865c29567e373056271
Kyouuka <piqia@qq.com>
2022-11-23 09:17:44 -0500

    add routers api list

--- shopping_service/flaskr/routers.py
+++ shopping_service/flaskr/routers.py
@@ -13,7 +13,7 @@ shopping_accessor = ShoppingAccessor()
 def home():
     return "shopping service home page"
 
-
+# CreateItemForSale
 @app.route('/create_item/<user_id>', methods=['POST'])
 def create_item(user_id):
     data = request.get_data()
@@ -25,14 +25,50 @@ def create_item(user_id):
     shopping_accessor.add_user_item(user_id=user_id, item_id=item_id)
 
 
+# AddItemToCart
 @app.route('/add_item_to_cart/<user_id>/<item_id>', methods=['PUT'])
 def add_item_to_cart(user_id, item_id):
     cart_id = shopping_accessor.get_current_cart(user_id=user_id)
     shopping_accessor.add_item_to_cart(cart_id=cart_id, item_id=item_id)
 
 
+# CheckoutItem
 @app.route('/checkout/<user_id>', methods=['PUT'])
 def checkout(user_id):
     cart_id = shopping_accessor.get_current_cart(user_id=user_id)
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     shopping_accessor.checkout_items(cart_id, current_time)
+
+
+# GetItemsInCartByUser
+@app.route('/get_items_in_cart/<user_id>', method=['PUT'])
+def get_items_in_cart(user_id):
+
+
+# RemoveItemFromCart
+@app.route('/remove_item_from_cart/<user_id>', method=['DELETE'])
+def remove_item_from_cart(user_id):
+
+
+# RemoveItemForSale
+@app.route('/remove_item_for_sale/<user_id>/<item_id>', method=['DELETE'])
+def remove_item_for_sale(user_id, item_id):
+
+
+# UpdateItemForSale
+@app.route('/update_item_for_sale', method=['POST'])
+def update_item_for_sale():
+
+
+# GetItemsForSaleByOwner
+@app.route('/get_items_for_sale/<user_id>', method=['GET'])
+def get_items_for_sale(user_id):
+
+
+# GetItemsByCategory
+@app.route('/get_items_by_category', methods=['GET'])
+def get_items_by_category():
+
+
+@app.route('/get_all_categories', methods=['GET'])
+def get_all_categories():
