45df51976010be321de1e8a08d6d04f9f20abe7b
yunerliu <liuyunchenyuner@gmail.com>
2022-11-23 19:53:57 -0500

    finish shopping inventory

--- shopping_service/flaskr/routers.py
+++ shopping_service/flaskr/routers.py
@@ -4,7 +4,7 @@ from common import err_msg
 from model.item import item_status
 from flask import Flask, request, jsonify
 import os
-import datetime
+from datetime import datetime
 import json
 import requests
 
@@ -40,11 +40,14 @@ def create_item():
     item['user_id'] = user_id 
     return pack_success(item)
 
+# GetItemsForSaleByOwner
 # http://127.0.0.1:5000/get_item?id=12345
 @app.route('/get_items_for_sale', methods=['GET'])
 def get_items_for_sale():
     user_id = request.args.get('user_id')
     item_ids, err = shopping_accessor.get_items_by_user(user_id)
+    if len(item_ids) == 0:
+        return pack_success(None)
     if err:
         return pack_err(err)
     item_ids = [str(item_id[0]) for item_id in item_ids]
@@ -92,7 +95,6 @@ def add_item_to_cart():
     shopping_accessor.add_item_to_cart(cart_id=cart_id, item_id=item_id, quantity=quantity)
     return pack_success(None)
 
-
 @app.route('/checkout', methods=['POST'])
 def checkout():
     data = request.get_data()
@@ -103,34 +105,49 @@ def checkout():
     except Exception as e:
         print(e)
         return pack_err(err_msg['db_err'])
-    # try:
-    #     items = shopping_accessor.get_items_from_cart(cart_id=cart_id)
-    #     item_id_list = [str(item_id[1]) for item_id in item_ids]
-    #     item_id_to_cnt = dict()    
-    # except Exception as e:
-    #     print(e)
-    #     return pack_err(err_msg['db_err'])  
-    # item_id_input = "_".join(item_id_list)
-    # r = requests.get(url=inventory_url + "/get_items?ids=" + item_id_input)
-    # items, err = parse_response(r)
-    # if err:
-    #     return pack_err(err)   
+    try:
+        items = shopping_accessor.get_items_from_cart(cart_id=cart_id)
+        item_id_list, consume_quantity_list = [], []
+        for item in items:
+            item_id_list.append(int(item[1]))
+            consume_quantity_list.append(int(item[2]))    
+    except Exception as e:
+        print(e)
+        return pack_err(err_msg['db_err'])   
+    inventory_input = {
+        'id_list':item_id_list,
+        'quantity_list':consume_quantity_list
+    }
+    inventory_input = json.dumps(inventory_input)
+    r = requests.post(inventory_url + "/comsume_availble_items", data=inventory_input)
+    result, err = parse_response(r)
+    if err:
+        return pack_err(err) 
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     try:
         shopping_accessor.checkout_items(cart_id, current_time)
     except Exception as e:
         print(e)
         return pack_err(err_msg['db_err'])
-    # # inventory_input = {
-    # #     'id':item_id
-    # # }
-    # inventory_input = json.dumps(inventory_input)
-    # r = requests.post(inventory_url + "/delete_item", data=inventory_input)
-    # _, err = parse_response(r)
-    # if err:
-    #     return pack_err(err)
-    return pack_success(None)    
-    
+    return pack_success(result)    
+
+# GetItemsInCartByUser
+@app.route('/get_items_in_cart', methods=['PUT'])
+def get_items_in_cart():
+    user_id = request.args.get('id')
+    cart_id = shopping_accessor.get_current_cart(user_id=user_id)
+    item_ids = shopping_accessor.get_items_from_cart(cart_id=cart_id)
+    return pack_success(item_ids)
+
+
+# RemoveItemFromCart
+@app.route('/remove_item_from_cart', methods=['DELETE'])
+def remove_item_from_cart():
+    user_id = request.args.get('id')
+    item_id = request.args.get('item')
+    cart_id = shopping_accessor.get_current_cart(user_id=user_id)
+    shopping_accessor.remove_item_from_cart(cart_id=cart_id, item_id=item_id)
+
 # ---------------- inner function ------------------
 def parse_response(resp):
     if resp.status_code != 200:
@@ -155,41 +172,6 @@ def pack_success(data):
         "status": True,
         "data": data
     })
-
-
-
-# GetItemsForSaleByOwner
-@app.route('/get_items_for_sale', methods=['GET'])
-def get_items_for_sale():
-    user_id = request.args.get('id')
-    item_ids = shopping_accessor.get_items_for_sale_by_user(user_id=user_id)
-    return pack_success(item_ids)
-
-
-# GetItemsInCartByUser
-@app.route('/get_items_in_cart', methods=['PUT'])
-def get_items_in_cart():
-    user_id = request.args.get('id')
-    cart_id = shopping_accessor.get_current_cart(user_id=user_id)
-    item_ids = shopping_accessor.get_items_from_cart(cart_id=cart_id)
-    return pack_success(item_ids)
-
-
-# RemoveItemFromCart
-@app.route('/remove_item_from_cart', methods=['DELETE'])
-def remove_item_from_cart():
-    user_id = request.args.get('id')
-    item_id = request.args.get('item')
-    cart_id = shopping_accessor.get_current_cart(user_id=user_id)
-    shopping_accessor.remove_item_from_cart(cart_id=cart_id, item_id=item_id)
-
-
-def pack_success(data):
-    return jsonify({
-        "status": True,
-        "data": data
-    })
-
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
