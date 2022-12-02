144e4aff8c461ce48b5d247ead9015bcec56fa87
Ted Wang <wei.s.wang@mail.mcgill.ca>
2022-12-01 21:04:17 -0800

    Fix CORS issue (https://enable-cors.org/server_flask.html)

--- account_service/app.py
+++ account_service/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from flaskr.container.dependency_container import Module
 from flaskr.router import account_routes
@@ -12,5 +13,5 @@ if __name__ == "__main__":
     di.wire(modules=[
         account_routes
     ])
-
+    cors = CORS(app)
     app.run(debug=True, host='0.0.0.0')
