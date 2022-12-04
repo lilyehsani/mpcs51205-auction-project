c8945a6e6643ab8ca76ea7754c9cfb361eca145c
Lily Ehsani <lilyehsani@uchicago.edu>
2022-12-03 21:41:57 -0600

    Uncomment create item page

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
+import CreateItem from "./components/CreateItem";
 import ItemList from "./components/ItemList";
 import ItemPage from "./components/ItemPage";
 import SignIn from "./components/SignIn";
@@ -16,7 +17,7 @@ function App() {
         <Route path={APP_ROUTES.SIGN_UP} exact element={<SignUp />} />
         <Route path={APP_ROUTES.SIGN_IN} element={<SignIn />} />
         <Route path={APP_ROUTES.DASHBOARD} element={<Dashboard />} />
-        {/* <Route path={APP_ROUTES.CREATE_ITEM} element={<CreateItem />} /> */}
+        <Route path={APP_ROUTES.CREATE_ITEM} element={<CreateItem />} />
         <Route path={APP_ROUTES.ITEM_LIST} element={<ItemList />} />
         <Route path={APP_ROUTES.ITEM_PAGE} element={<ItemPage />} />
       </Routes>
